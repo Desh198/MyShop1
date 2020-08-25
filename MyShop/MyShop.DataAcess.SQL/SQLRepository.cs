4bccb8bf15c2ddb92243a169f801c6e7fe960f7f
Desh198 <67443246+Desh198@users.noreply.github.com>
2020-08-25 20:29:42 +0200

    Implemented generic Repository

---
+++ MyShop/MyShop.DataAcess.SQL/SQLRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using MyShop.Core.Contacts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAcess.SQL
+{
+    public class SQLRepository<T>: IRepository<T> where T: BaseEntity
+    {
+
+    }
+}
